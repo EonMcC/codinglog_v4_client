9da8be2d8352c566117ad192aa9a214599a6fef5
Iain <iain2eyes@gmail.com>
2020-04-14 04:50:13 +0100

    Add an Add Time button to language

--- src/components/LanguageItem.js
+++ src/components/LanguageItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/languages-style.css";
+import { Link } from "react-router-dom";
 
 const LanguageItem = ({ text, totalTime, removeLanguage, reloadLanguages }) => {
   const timeMins = Math.round(totalTime / 3600000);
@@ -21,8 +22,12 @@ const LanguageItem = ({ text, totalTime, removeLanguage, reloadLanguages }) => {
       {timeMins >= 1 && timeMins < 2 && <p>hour</p>}
       {timeMins >= 2 && <p>hours</p>}
       {timeMins < 1 && <p>less than an hour, keep coding!</p>}
-
-      <button onClick={removeReload}>Delete Language</button>
+      <div className="language-button-container">
+        <button>
+          <Link to="/manual-list">Add Time</Link>
+        </button>
+        <button onClick={removeReload}>Delete Language</button>
+      </div>
     </div>
   );
 };
